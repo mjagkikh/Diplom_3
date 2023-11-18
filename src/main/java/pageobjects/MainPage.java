a4382940f32d93d4e091e2ee473f3e1a927358b5
Andrei Miagkikh <andsm93@mail.ru>
2023-11-18 21:33:33 +0700

    Constructor tests added

--- src/main/java/pageobjects/MainPage.java
+++ src/main/java/pageobjects/MainPage.java
@@ -14,9 +14,13 @@ public class MainPage {
     private static final String MAIN_PAGE_URL = "https://stellarburgers.nomoreparties.site";
     private final By makeOrderButton = By.xpath(".//button[text()='Оформить заказ']");
     private final By loginButtonOnMainPage = By.xpath(".//button[text()='Войти в аккаунт']");
-
     private final By accountButtonOnMainPage = By.xpath(".//a[@href='/account']");
-
+    private final By sauceButtonOnMainPage = By.xpath(".//span[text()='Соусы']");
+    private final By fillingButtonOnMainPage = By.xpath(".//span[text()='Начинки']");
+    private final By bunsButtonOnMainPage = By.xpath(".//span[text()='Булки']");
+    private final By sauceSection = By.xpath(".//h2[text()='Соусы']");
+    private final By fillingSection = By.xpath(".//h2[text()='Начинки']");
+    private final By bunsSection = By.xpath(".//h2[text()='Булки']");
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
@@ -40,5 +44,24 @@ public class MainPage {
     @Step("Click on account button on main page")
     public void clickOnAccountButton() {
         driver.findElement(accountButtonOnMainPage).click();
+    }
+
+    @Step("Click sauce button on main page, check sauce")
+    public boolean checkSauceIsDisplayed() {
+        driver.findElement(sauceButtonOnMainPage).click();
+        return driver.findElement(sauceSection).isDisplayed();
+    }
+
+    @Step("Click filling button on main page")
+    public boolean checkFillingIsDisplayed() {
+        driver.findElement(fillingButtonOnMainPage).click();
+        return driver.findElement(fillingSection).isDisplayed();
+    }
+
+    @Step("Click buns button on main page")
+    public boolean checkBunsIsDisplayed() {
+        driver.findElement(sauceButtonOnMainPage).click();
+        driver.findElement(bunsButtonOnMainPage).click();
+        return driver.findElement(bunsSection).isDisplayed();
     }
 }
